f299700234b0be7efca95d15ac78e5e3e76c36ca
anne jhetson <student@epicodus.com>
2014-03-10 11:55:37 -0700

    Arguments received from Terminal (STDIN).

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -15,7 +15,38 @@ class Parcel
   end
 end
 
-test_parcel = Parcel.new(1, 12, 24, 5)
+
+# Method #2
+# Run: ruby lib/parcel.rb in terminal and hit Enter
+
+print "Enter weight "
+arg1 = gets.chomp
+
+print "Enter width "
+arg2 = gets.chomp
+
+print "Enter length "
+arg3 = gets.chomp
+
+print "Enter height: "
+arg4 = gets.chomp
+
+test_parcel = Parcel.new(arg1.to_i, arg2.to_i, arg3.to_i, arg4.to_i)
 test_parcel.volume
-# puts test_parcel.volume
 puts test_parcel.shipping_cost
+
+
+# Method #1
+# Run: ruby lib/parcel.rb 23 34 12 43
+
+# arg1 = ARGV[0]
+
+# arg2 = ARGV[1]
+
+# arg3 = ARGV[2]
+
+# arg4 = ARGV[3]
+
+# test_parcel = Parcel.new(arg1.to_i, arg2.to_i, arg3.to_i, arg4.to_i)
+# test_parcel.volume
+# puts test_parcel.shipping_cost
